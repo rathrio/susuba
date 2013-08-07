ee2259a77b885b4a7166f24acffbb40da455616a
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-08-07 22:34:59 +0200

    Initial Commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject susuba "0.1.0-SNAPSHOT"
+  :description "Susuba Backend"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5"]]
+  :plugins [[lein-ring "0.8.5"]]
+  :ring {:handler susuba.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
