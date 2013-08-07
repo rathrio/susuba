63e4a76ea5b493d334d53e510440f7c160906a1f
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-08-08 00:29:31 +0200

    Added main function to main handler

--- project.clj
+++ project.clj
@@ -2,9 +2,11 @@
   :description "Susuba Backend"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+  :main susuba.handler
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
-                 [congomongo "0.4.1"]]
+                 [congomongo "0.4.1"]
+                 [ring "1.2.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler susuba.handler/app}
   :profiles
--- src/susuba/handler.clj
+++ src/susuba/handler.clj
@@ -1,5 +1,6 @@
 (ns susuba.handler
-  (:use compojure.core)
+  (:use compojure.core
+        [ring.adapter.jetty :only [run-jetty]])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
@@ -10,3 +11,9 @@
 
 (def app
   (handler/site app-routes))
+
+(defn- get-port []
+  (Integer/parseInt (or (System/getenv "PORT") "3000")))
+
+(defn -main []
+  (run-jetty app {:port (get-port)}))
