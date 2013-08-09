578364ec89487dde8485b22a508dbe659bd92f34
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-08-10 00:42:05 +0200

    Using correct mongoenv

--- src/susuba/db.clj
+++ src/susuba/db.clj
@@ -7,7 +7,7 @@
   "Connects to a collection if it exists, creates it otherwise."
   []
   (when-not (m/connection? *mongo-config*)
-    (let [mongo-url (get (System/getenv) "MONGODB_URI" "mongodb://localhost/susuba")]
+    (let [mongo-url (get (System/getenv) "MONGOHQ_URL" "mongodb://localhost/susuba")]
       (m/set-connection! (m/make-connection mongo-url))
       (m/set-write-concern *mongo-config* :safe)
       (beer/maybe-init))))
