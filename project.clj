fa8032986071054de1d7267e90ded8ce1d5c4997
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2013-08-10 00:22:34 +0200

    Introduced API. No longer full site.

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [congomongo "0.4.1"]
-                 [ring "1.2.0"]]
+                 [cheshire "5.2.0"]
+                 [ring "1.2.0"]
+                 [ring/ring-json "0.2.0"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler susuba.handler/app}
   :profiles
--- src/susuba/handler.clj
+++ src/susuba/handler.clj
@@ -1,19 +1,25 @@
 (ns susuba.handler
   (:use compojure.core
-        [ring.adapter.jetty :only [run-jetty]])
+        [ring.adapter.jetty :only [run-jetty]]
+        [ring.middleware.json]
+        [ring.util.response])
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
             [susuba.db :as db]
             [susuba.beer :as beer]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (GET "/beers" [] (beer/all))
+  (context "/beers" [] (defroutes beer-routes
+    (GET "/" [] (beer/all))))
+
+  (GET "/" [] "Hello this is dawg.")
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (handler/site app-routes))
+  (-> (handler/api app-routes)
+    (wrap-json-body)
+    (wrap-json-response)))
 
 (defn- port []
   (Integer/parseInt (or (System/getenv "PORT") "3000")))
